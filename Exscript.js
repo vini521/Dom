ec467f7495c3af011c0eb39c2cce0a5528f7e046
vini521 <vinicius.desidera@aluno.senai.br>
2025-04-03 07:31:07 -0300

    Exercicio 2 DOM

--- Exscript.js
+++ Exscript.js
@@ -12,3 +12,20 @@ console.log("O esporte selecionado por Query Selector com o id Ten é: " + idQS.
 
 let claQS = document.querySelector(".Ca");
 console.log("O esporte selecionado por Query selector com a class Basca é: " + claQS.textContent);
+
+//pegando o id da ul
+let lista = document.getElementById("lista");
+
+let h1 = document.getElementById("h1");
+h1.innerHTML = "<span style='color:white'> Meus Esportes Favoritos </span>";
+
+let novaLi = document.createElement("li");
+novaLi.innerHTML = "PingPong";
+
+lista.appendChild(novaLi);
+
+let Li2 = document.createElement("li");
+Li2.innerHTML = "Levantamento de peso";
+
+//adicionando a li a ul
+lista.appendChild(Li2);
